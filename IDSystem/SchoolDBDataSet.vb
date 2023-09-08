52f09964d152e9c9686fe0a4675069b947dfd6fb
lukeperry <lukezabala@gmail.com>
2023-09-09 06:40:06 +0800

    Add files via upload

---
+++ IDSystem/SchoolDBDataSet.vb
@@ -0,0 +1,17 @@
+﻿Partial Class SchoolDBDataSet
+End Class
+
+Namespace SchoolDBDataSetTableAdapters
+    
+    Partial Public Class UsersTableAdapter
+
+        Function GetDataByUserLogin(ByVal p1 As String, ByVal p2 As String) As Object
+            Throw New NotImplementedException
+        End Function
+
+        Function GetDataByUserLogin(ByVal p1 As String) As Object
+            Throw New NotImplementedException
+        End Function
+
+    End Class
+End Namespace
